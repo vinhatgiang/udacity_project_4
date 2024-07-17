4f98f892586f1539ea3a6efc390a9978d8ac54ba
vinhatgiang <vinhatgiang@gmail.com>
2024-07-17 14:13:01 +0000

    fix bug

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -3,27 +3,27 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
-  }, []);
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+            setMovies(response.data.movies);
+        });
+    }, []);
 
-  return (
-    <ul>
-      {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
-          {movie.title}
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul>
+            {movies.map((movie) => (
+                <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+                    {movie.title};
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 MovieList.propTypes = {
-  onMovieClick: PropTypes.func.isRequired,
+    onMovieClick: PropTypes.func.isRequired,
 };
 
 export default MovieList;
